7b2cf9761eab94b74fe5846bd9a5d371c993279c
Алексеева Анастасия Николаевна <analekseeva_1@edu.hse.ru>
2017-08-05 13:53:24 +0500

    начала прописывать создание списка и его вывод

---
+++ Task9/Point.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Task9
+{
+    public class Point // однонаправленный список
+    {
+        public string data;
+        public Point next;
+        public Point(string d)
+        {
+            data = d;
+            next = null;
+        }
+        public override string ToString()
+        {
+            return data + " ";
+        }
+
+        public static Point MakePoint(string d) // добавление элемента в список
+        {
+            Point p = new Point(d);
+            return p;
+        } // конец MakePoint
+
+        public static Point MakeList(int size) // создание элементов списка
+        {
+            if (size == 0) return null;
+            Console.WriteLine("Введите элемент");
+            string info = Console.ReadLine();
+            Point beg = MakePoint(info);
+            Point r = beg;
+            for (int i = 1; i < size; i++)
+            {
+                Console.WriteLine("Введите элемент");
+                info = Console.ReadLine();
+                Point p = MakePoint(info);
+                r.next = p;
+                r = p;
+            }
+            return beg;
+        } // конец MakeList
+
+        public static void ShowPoint(Point beg) // печать списка
+        {
+            if (beg == null)
+            {
+                Console.WriteLine("Cписок пуст");
+                return;
+            }
+            Point p = beg;
+            Console.Write("Элементы списка: ");
+            while (p != null)
+            {
+                Console.Write(p);
+                p = p.next;
+            }
+            Console.WriteLine();
+        } // конец ShowList
+
+        public static void DeleteElement()
+        {
+            
+        }
+
+        public static void SearchElement()
+        {
+            
+        }
+    } // конец Point
+}
