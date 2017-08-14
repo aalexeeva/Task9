5d5cee4eef8fa1b2e02557d1d3a6041c80d34198
Алексеева Анастасия Николаевна <analekseeva_1@edu.hse.ru>
2017-08-14 15:06:48 +0500

    добавлены комментарии

--- Task9/Program.cs
+++ Task9/Program.cs
@@ -7,7 +7,7 @@ namespace Task9
     {
         public static int Input(string text, int minSize, int maxSize) // проверка соответствия ввода типу int
         {
-            var number = 0; 
+            var number = 0; // переменная для числа
             bool ok;
             var word = " ";
             var size = new string[2];
@@ -17,9 +17,9 @@ namespace Task9
             {
                 try
                 {
-                    WriteLine(text);
+                    WriteLine(text); // вывод сообщения
                     number = Convert.ToInt32(ReadLine());
-                    if (number >= minSize && number <= maxSize) ok = true;
+                    if (number >= minSize && number <= maxSize) ok = true; // проверка вхождения числа в допустимый диапазон
                     else
                     {
                         if (number >= maxSize) word = size[0];
@@ -64,25 +64,28 @@ namespace Task9
             do
             {
                 var n = Input("Введите количество элементов списка:", 0, 2147483647);
-                Point beg = Point.MakeList(n);
+                Point beg = Point.MakeList(n); // создание списка
+                // вывод меню
                 var userAnswer =
-                    Input("Выберите, что сделать со списком: \n1.Вывести список\n2.Удалить элемент\n3.Найти элемент", 0,
-                        4);
+                    Input("Выберите, что сделать со списком: \n1.Вывести список\n2.Удалить элемент\n3.Найти элемент\n4.Выход", 0, 5);
                 switch (userAnswer)
                 {
-                    case 1:
+                    case 1: // вывод списка
                         Point.ShowPoint(beg);
                         break;
-                    case 2:
+                    case 2: // удаление элемента
                         var number = Input("Введите элемент, который необходимо удалить:", 0, 2147483647);
                         Point.DeleteElement(number, beg);
                         break;
-                    case 3:
+                    case 3: // поиск элемента
                         var number2 = Input("Введите элемент, который необходимо найти:", 0, 2147483647);
                         Point.SearchElement(number2, beg);
                         break;
+                    case 4: // выход
+                        Environment.Exit(0);
+                        break;
                 }
-                okay = Exit();
+                okay = Exit(); // выход из программы
             } while (!okay);
         }
     }
